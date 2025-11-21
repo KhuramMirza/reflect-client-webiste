1c3c5992e3cfed6c4c02f39e37e11364680773fb
Khuram Mirza <mirzakhuram101@gmail.com>
2025-11-22 00:57:35 +0500

    feat(layout): separate auth, public, and user layouts inside their own Route Groups

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
 
---
+++ app/(user)/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div>This is your Dashboard</div>;
+}
