bb61e163d5aa97682f13d1f6be81cfd56362bf32
Khuram Mirza <mirzakhuram101@gmail.com>
2025-11-22 00:16:00 +0500

    feat(auth/api): add signupUser api function

--- features/auth/api.ts
+++ features/auth/api.ts
@@ -1,10 +1,21 @@
 import { fetchApi } from "@/lib/fetcher";
-import { Login } from "@/features/auth/types";
+import { Login, Signup } from "@/features/auth/types";
 
 export async function loginUser(credentials: Login) {
   return await fetchApi("/auth/login", {
     method: "POST",
     body: JSON.stringify(credentials),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+}
+
+export async function signupUser(details: Signup) {
+  return await fetchApi("/auth/signup", {
+    method: "POST",
+    body: JSON.stringify(details),
     headers: {
       "Content-Type": "application/json",
     },
