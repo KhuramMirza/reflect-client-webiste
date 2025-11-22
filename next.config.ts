ee22882f45e2f9c22d887dc3b518625295985e8e
Khuram Mirza <mirzakhuram101@gmail.com>
2025-11-22 22:17:34 +0500

    feat(config): add cloudinary config files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dluq2gdvu/**",
+      },
+    ],
+
+    qualities: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+  },
 };
 
 export default nextConfig;
