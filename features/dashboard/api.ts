9ce1f12b7ec12c79f6ee02c30cd493b2375c921d
Khuram Mirza <mirzakhuram101@gmail.com>
2025-11-23 16:16:25 +0500

    feat(dashboard/api): create fetchRecentlyGeneratedQuizzes, fetchQuiz, submitQuiz, and getQuizAttempt api functions

--- features/dashboard/api.ts
+++ features/dashboard/api.ts
@@ -1,5 +1,6 @@
 import { fetchApi } from "@/lib/fetcher";
 import { GenerateQuiz } from "@/features/dashboard/types";
+import { getAuthToken } from "@/features/auth/token";
 
 export async function generateQuiz(token: string, body: GenerateQuiz) {
   return await fetchApi(`/quiz/generate-simple`, {
@@ -8,5 +9,46 @@ export async function generateQuiz(token: string, body: GenerateQuiz) {
       Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(body),
+  });
+}
+
+export async function fetchRecentlyGeneratedQuizzes() {
+  const token = await getAuthToken();
+  return await fetchApi(`/quiz/get-recently-generated`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+export async function fetchQuiz(id: string) {
+  const token = await getAuthToken();
+  return await fetchApi(`/quiz/${id}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+export async function submitQuiz(id: string, submittedAnswers: string[]) {
+  const token = await getAuthToken();
+  return await fetchApi(`/quiz/${id}/submit`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(submittedAnswers),
+  });
+}
+
+export async function getQuizAttempt(quizId: string) {
+  const token = await getAuthToken();
+  return await fetchApi(`/quiz/attempt/${quizId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
