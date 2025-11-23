3b2e1825704fac2769d7bb43e5c559e7bb93f41a
Khuram Mirza <mirzakhuram101@gmail.com>
2025-11-23 16:15:10 +0500

    feat(dashboard/types): create SimpleQuizData, Question, QuizDoc, and ApiResponse types

--- features/dashboard/types.ts
+++ features/dashboard/types.ts
@@ -14,3 +14,29 @@ export type QuizGenerationResult = {
   status: string;
   message: string;
 };
+
+export type SimpleQuizData = {
+  id: string;
+  quizTopic: string;
+  difficulty: string;
+  questionsCount: number;
+};
+
+export interface Question {
+  _id: string;
+  questionText: string;
+  options: string[];
+}
+
+export interface QuizDoc {
+  _id: string;
+  quizTopic: string;
+  questions: Question[];
+}
+
+export interface ApiResponse {
+  status: string;
+  data: {
+    doc: QuizDoc;
+  };
+}
