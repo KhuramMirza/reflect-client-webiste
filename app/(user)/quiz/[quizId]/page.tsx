102f8ea9b5b9542dabd8f973b0bda1305af56ddb
Khuram Mirza <mirzakhuram101@gmail.com>
2025-11-23 17:00:25 +0500

    feat(page/quiz): create Quiz Attempt Page that takes quiz id as params fetches the quiz and pass it to QuizForm

---
+++ app/(user)/quiz/[quizId]/page.tsx
@@ -0,0 +1,21 @@
+import { fetchQuiz } from "@/features/dashboard/api";
+import QuizForm from "@/features/dashboard/components/QuizForm";
+
+interface PageProps {
+  params: {
+    quizSlug: string;
+  };
+}
+
+export default async function QuizPage({ params }: Readonly<PageProps>) {
+  const { quizId } = await params;
+
+  const response = await fetchQuiz(quizId);
+  const quizDoc = response.data.doc;
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <QuizForm quiz={quizDoc} />
+    </div>
+  );
+}
