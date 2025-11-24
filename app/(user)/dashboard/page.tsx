a8b62ebdad4a31952f891fd94b7bda855ff828ec
Khuram Mirza <mirzakhuram101@gmail.com>
2025-11-24 23:56:24 +0500

    feat(dashboard/page): import quiz and roadmap sections inside of dashboard page

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <div>This is your Dashboard</div>;
+import DashboardHero from "@/features/dashboard/components/DashboardHero";
+import QuizzesSection from "@/features/dashboard/components/QuizzesSection";
+import GeneratedQuizzesSection from "@/features/dashboard/components/GeneratedQuizzesSection";
+import RoadmapsSection from "@/features/roadmap/components/RoadmapsSection";
+import GeneratedRoadmapsSection from "@/features/roadmap/components/GeneratedRoadmapsSection";
+
+export default async function Page() {
+  return (
+    <div className="mt-10">
+      <DashboardHero />
+
+      <QuizzesSection>
+        <GeneratedQuizzesSection />
+      </QuizzesSection>
+
+      <RoadmapsSection>
+        <GeneratedRoadmapsSection />
+      </RoadmapsSection>
+    </div>
+  );
 }
