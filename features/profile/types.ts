ae59eb63051109cb4fd08e6c926e59e53a4822aa
Khuram Mirza <mirzakhuram101@gmail.com>
2025-11-26 00:48:34 +0500

    feat(profile/types): create UserProps, and UserData interfaces

---
+++ features/profile/types.ts
@@ -0,0 +1,17 @@
+export interface UserData {
+  _id: string;
+  name: string;
+  email: string;
+  profileImage: string;
+  bio?: string;
+  role: string;
+}
+
+export interface UserProps {
+  _id: string;
+  name: string;
+  email: string;
+  profileImage: string;
+  bio?: string;
+  role: string;
+}
